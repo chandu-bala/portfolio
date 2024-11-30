a8bf88bf55a5e45c1249aaf3603c3168b920cd23
Chandu <chandududdela0103@gmail.com>
2024-11-30 11:38:43 +0530

    my commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          // Define any custom colors if needed
+          tertiary: '#f39c12', // Example color
+        },
+      },
+    },
+    plugins: [],
+  };
+  

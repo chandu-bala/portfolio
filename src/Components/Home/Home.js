529db48b6d4f27f588820f132355037f02d65716
Chandu <chandududdela0103@gmail.com>
2025-12-26 16:40:56 +0530

    Assets - Upt

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/MyPic.png";
+import homeLogo from "../../Assets/MyPic1.png";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
